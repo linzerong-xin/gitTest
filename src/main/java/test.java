1e6647db7282339bf7126bc94b7701763d812389
linzexin <1740303648@qq.com>
2022-05-20 18:20:33 +0800

    hot-fix1提交

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hell01");
+        System.out.println("hot-fix");
     }
 }

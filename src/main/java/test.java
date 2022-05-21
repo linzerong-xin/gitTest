de8f5b08e8f1a423f04dd219a2a06c5f034405da
linzerong-xin <105252474+linzerong-xin@users.noreply.github.com>
2022-05-21 09:02:25 +0800

    pull test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,5 +5,6 @@ public class test {
         System.out.println("hot-fix");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
